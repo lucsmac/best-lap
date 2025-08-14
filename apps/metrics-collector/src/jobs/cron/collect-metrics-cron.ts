1f0217a97bcda71e3212ba3fb6bc05af3a0bf8b9
Lucas Macedo <lucas@autoforce.com>
2025-08-14 09:28:20 -0300

    feat: change service cron

--- apps/metrics-collector/src/jobs/cron/collect-metrics-cron.ts
+++ apps/metrics-collector/src/jobs/cron/collect-metrics-cron.ts
@@ -2,7 +2,7 @@ import cron from 'node-cron';
 import { makeCollectPageMetricsDispatcher } from '../../factories';
 
 export const collectMetricsCron = () => {
-  cron.schedule('*/30 * * * *', async () => {
+  cron.schedule('0 8,14,20 * * *', async () => {
     const collectMetricsJobDispatcher = makeCollectPageMetricsDispatcher()
     await collectMetricsJobDispatcher.execute();
   });
